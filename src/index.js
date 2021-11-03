f7bbe8931a6572936052ec89e51db6ad5843aa31
Igor Bobeldijk <igor.bobeldijk@gmail.com>
2021-11-03 15:08:38 +0100

    blabla

--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
 
 import * as Tone from "tone";
 
-const compass = document.getElementById("compass");
 let heading;
+let direction;
 
 function init() {
-    compass.innerHTML = "iPhone???";
-
-    const button = document.createElement("button");
-    button.innerHTML = "click me";
-
-    document.body.appendChild(button);
-
-    var other_button = document.getElementById("new-direction-button");
-    other_button.remove();
-
+    const button = document.getElementById("new-direction-button");
+    //const button = document.createElement("button");
+    button.innerHTML = "START COMPASS";
 
     //const button = document.getElementById("new-direction-button");
     button.addEventListener("click", startCompass);
 }
 
 function startCompass() {
+    const button = document.getElementById("new-direction-button");
+    button.removeEventListener("click", startCompass);
     var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 
     if (isIOS) {
         DeviceOrientationEvent.requestPermission()
             .then((response) => {
             if (response === "granted") {
-                compass.innerHTML = "granted"
                 window.addEventListener("deviceorientation", handler, true);
             } else {
                 alert("has to be allowed!");
@@ -37,12 +31,21 @@ function startCompass() {
     } else {
         window.addEventListener("deviceorientationabsolute", handler, true);
     }
+    button.innerHTML = "CHANGE DIRECTION";
+    button.addEventListener("click", changeDirection);
+}
+
+function changeDirection() {
+    direction = Math.random() * 360;
+    var direction_arrow = document.getElementById("direction-arrow");
+    direction_arrow.style.transform = `rotate(${-direction}deg)`;
 }
 
 function handler(e) {
     heading = e.webkitCompassHeading || Math.abs(e.alpha - 360);
-    compass.innerHTML = heading;
-    compassCircle.style.transform = `translate(-50%, -50%) rotate(${-compass}deg)`;
+    diff_heading = Math.abs(heading - direction);
+    var compass_arrow = document.getElementById("compass-arrow");
+    compass_arrow.style.transform = `rotate(${-diff_heading}deg)`;
 }
 
 init();
